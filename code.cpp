f9c473ad4f9e3949b747dfb26c905e71a337d6b0
Khadija Saeed <144422229+khadijasaeed683@users.noreply.github.com>
2023-09-18 15:06:24 +0500

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout << "enter amount in dollor" << endl;
+	int dollor;
+	cin >> dollor;
+	int rupee;
+	rupee = 200*dollor;
+	cout << rupee;
+}
---
+++ task1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	string name;
+	cout << "enter name: ";
+	cin >> name;
+	cout << "enter roll number: ";
+	int roll;
+	cin >> roll;
+	cout << "enter section: ";
+	char section;
+	cin >> section;
+	cout << "student information" << endl;
+	cout << "name: " << name <<endl;
+	cout << "roll number: " << roll <<endl;
+	cout << "section: " << section << endl;
+
+
+
+
+}
